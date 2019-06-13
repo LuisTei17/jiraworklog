2f62e57a4486303eb0735928e182266987ccf090
Luis Teixeira <luisedteixeira@gmail.com>
2019-06-13 16:58:40 -0300

    making auth

---
+++ app/components/auth/authRoutes.js
@@ -0,0 +1,19 @@
+const joi = require('joi'),
+    handler = require('./authControllers');
+
+module.exports = [
+    {
+        'path':'/auth/',
+        'method': 'POST',
+        'handler': handler.makeAuth,
+        'config': {
+            'description': 'Download a file',
+            'validate': {
+                'payload': {
+                    'username': joi.string().required(),
+                    'password': joi.string().required()
+                }
+            }
+        }
+    }
+];
